7f028d503dd07af026bc1bc89e2184ee922b431e
Samuel Mbugua <sthaiya@hotmail.com>
2010-03-16 13:41:23 +0000

    amrsmobileforms: Initial commit of version 1.0.1

--- src/org/openmrs/module/amrsmobileforms/util/MobileFormEntryFileUploader.java
+++ src/org/openmrs/module/amrsmobileforms/util/MobileFormEntryFileUploader.java
@@ -19,7 +19,7 @@ public class MobileFormEntryFileUploader {
 	public static void submitXFormFile(String filePath) throws IOException, Exception {
 
 		File file = new File(filePath);
-		XformDataUploadManager.processXform(OpenmrsUtil.getFileAsString(file), getRandomSessionString(), XformsUtil.getEnterer());
+		XformDataUploadManager.processXform(OpenmrsUtil.getFileAsString(file), getRandomSessionString(), XformsUtil.getEnterer(),false);
 	}
 	
 	/**
