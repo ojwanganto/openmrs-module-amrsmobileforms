4eb565b075e83d5b2277562c006bb2ba95d79bc4
Samuel Mbugua <sthaiya@hotmail.com>
2010-05-04 11:59:08 +0000

    amrsmobileforms: Minor changes

--- src/org/openmrs/module/amrsmobileforms/MobileFormPostProcessor.java
+++ src/org/openmrs/module/amrsmobileforms/MobileFormPostProcessor.java
@@ -1,7 +1,6 @@
 package org.openmrs.module.amrsmobileforms;
 
 import java.io.File;
-import java.util.Date;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
