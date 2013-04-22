06b68a2eeed1bc63b30520d623c71d396d279e16
ojwanganto <ojwangwachiaje@gmail.com>
2013-04-22 12:03:28 +0300

    MOB_24 : Cleaned up code to only edit a file when content has changed

--- omod/src/main/java/org/openmrs/module/amrsmobileforms/web/ParseXML.groovy
+++ omod/src/main/java/org/openmrs/module/amrsmobileforms/web/ParseXML.groovy
@@ -8,32 +8,36 @@ public class ParseXML{
         new File(filePath).eachFile() { file->
 
             def fullFilePath = filePath +"/" + file.getName()
-            def fileData = new XmlSlurper().parse(fullFilePath);
+            def fileData = new XmlSlurper(false,false).parse(fullFilePath);
 
             if(fileData==null){
-                //todo:
+                //todo: handle blank files
             }
             else{
 
 
                 def individuals = fileData.household.individuals.individual
-                //def petientNo = patient.medical_record_number
+
                 individuals.each{
                     String patient_number = it.patient."patient.medical_record_number"
-
+                    patient_number = patient_number.toString()
 
                     if(patient_number){
 
                         patient_number.trim()
 
-                        patient_number.replaceAll(/ *$/, '')
+                        String newNum =patient_number.replaceAll("\\s","")
 
-                        it.patient."patient.medical_record_number".replaceBody(patient_number)
+                        if(!patient_number.equals(newNum)){
 
-                        def writable = new StreamingMarkupBuilder().bind { mkp.yield fileData }
-                        writable.writeTo(new PrintWriter(new FileWriter(fullFilePath)))
+                            it.patient."patient.medical_record_number".replaceBody(newNum)
 
-                        println "Patient Identifier: " + it.patient."patient.medical_record_number"
+                            def writable = new StreamingMarkupBuilder().bind { mkp.yield fileData }
+                            writable.writeTo(new PrintWriter(new FileWriter(fullFilePath)))
+
+
+                        }
+
 
                     }
                     else{
