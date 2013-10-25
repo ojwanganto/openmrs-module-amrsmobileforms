ff81424750ae83a643732395055ad99527963547
ojwanganto <ojwangwachiaje@gmail.com>
2013-10-25 16:41:26 +0300

    Changes to run groovy using spring framework

--- api/src/main/java/org/openmrs/module/amrsmobileforms/DeleteSpacesInPatiendIdTask.java
+++ api/src/main/java/org/openmrs/module/amrsmobileforms/DeleteSpacesInPatiendIdTask.java
@@ -4,13 +4,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.api.APIException;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.amrsmobileforms.groovy.CleanPatientIdentifiers;
 import org.openmrs.module.amrsmobileforms.util.MobileFormEntryUtil;
 import org.openmrs.scheduler.tasks.AbstractTask;
-import groovy.lang.GroovyShell ;
-import groovy.lang.GroovyClassLoader ;
-import groovy.util.GroovyScriptEngine ;
-import java.io.File ;
-import java.io.IOException;
 
 /**
  * A task to clean up patient identifier prior to processing.
@@ -21,9 +17,17 @@ public class DeleteSpacesInPatiendIdTask extends AbstractTask {
 
 	private static Log log = LogFactory.getLog(DeleteSpacesInPatiendIdTask.class);
     String filePath = MobileFormEntryUtil.getMobileFormsDropDir().getAbsolutePath();
+    private CleanPatientIdentifiers cleanPatientIdentifiers;
 
+    public CleanPatientIdentifiers getCleanPatientIdentifiers() {
+        return cleanPatientIdentifiers;
+    }
 
-	/**
+    public void setCleanPatientIdentifiers(CleanPatientIdentifiers cleanPatientIdentifiers) {
+        this.cleanPatientIdentifiers = cleanPatientIdentifiers;
+    }
+
+    /**
 	 * Call Groovy class that cleans patient identifiers
 	 */
 	public void execute() {
@@ -33,14 +37,11 @@ public class DeleteSpacesInPatiendIdTask extends AbstractTask {
 			if (Context.isAuthenticated() == false)
 				authenticate();
             //It would be good if we pass the full URL to path of the directory with forms to be processed
-            new GroovyShell().parse( new File( "groovy/ParseXML.groovy" ) ).invokeMethod("getDirFileListing", filePath) ;
+            cleanPatientIdentifiers.getDirFileListing(filePath);
 		} catch (APIException e) {
 			log.error("Error running mobile forms relationships task", e);
 			throw e;
-		} catch (IOException e) {
-            e.printStackTrace();
-            log.error("File error occurred during processing");
-        } finally {
+		} finally {
 			Context.closeSession();
 		}
 	}
@@ -52,4 +53,6 @@ public class DeleteSpacesInPatiendIdTask extends AbstractTask {
 		super.shutdown();
 		log.debug("Shutting down task that cleans Patient Identifier");
 	}
+
+
 }
---
+++ api/src/main/java/org/openmrs/module/amrsmobileforms/groovy/CleanPatientIdentifiers.java
@@ -0,0 +1,7 @@
+package org.openmrs.module.amrsmobileforms.groovy;
+
+/**
+ * TODO:please provide a brief description for the class.
+ */
+public interface CleanPatientIdentifiers {
+}
